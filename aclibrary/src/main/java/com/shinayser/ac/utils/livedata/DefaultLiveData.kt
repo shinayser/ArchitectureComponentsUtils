048ea4aec9da50f752f77747a0c0e451906b3dc5
denox-PC\denox <danieloliveira@denox.com.br>
2017-12-13 14:38:27 -0200

    Added function alter to defautl live datas

--- aclibrary/src/main/java/com/shinayser/ac/utils/livedata/DefaultLiveData.kt
+++ aclibrary/src/main/java/com/shinayser/ac/utils/livedata/DefaultLiveData.kt
@@ -22,12 +22,12 @@ open class DefaultLiveData<T>() : MutableLiveData<T>() {
      * WARNING: if calling it from JAVA instead of Kotlin, be aware that the primitive live datas (IntLiveData, DoubleLiveData, FloatLiveData, LongLiveData, BooleanLiveData)
      * cannot be assign to null, so, never return null from the block function.
      */
-    open fun modify(block: (T) -> T): T {
+    open fun setValueTo(block: (T) -> T): T {
         this.value = block(this.value)
         return this.value
     }
 
-    open fun alter(block: T.() -> Unit): Unit {
+    open fun modify(block: T.() -> Unit): Unit {
         this.value.block()
         this.value = this.value
     }
